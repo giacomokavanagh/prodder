fd344c41b8386c016e007cfcde5748fe0d512380
Giacomo Kavanagh <giacomokavanagh@gmail.com>
2016-02-14 21:07:35 +0000

    Initial.

---
+++ Prodder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prodder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest ResetWaiting, ResetFailed;
+
+            var strHostAddress = Environment.GetEnvironmentVariable("AF_HOST_ADDRESS");
+
+            ResetWaiting = WebRequest.Create(strHostAddress + "TestRuns/ResetWaitingOrBlockedTestRuns");
+            ResetWaiting.GetResponse().GetResponseStream();
+
+            ResetFailed = WebRequest.Create(strHostAddress + "TestRuns/ResetFailedTests");
+            ResetFailed.GetResponse().GetResponseStream();
+        }
+    }
+}
